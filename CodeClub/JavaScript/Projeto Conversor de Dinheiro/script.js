233b7cafdaf8abcfb3e48f613a775986c9e87998
Alexsandro Gomes Clemente <alexsandro.gomes.clemente@hotmail.com>
2021-12-16 23:04:50 -0300

    Projeto

--- CodeClub/JavaScript/Projeto Conversor de Dinheiro/script.js
+++ CodeClub/JavaScript/Projeto Conversor de Dinheiro/script.js
@@ -1,11 +1,40 @@
 const value = document.getElementById('calcValue')
-const dolar = 5.68 
 const calc = document.getElementById('button')
 const Span = document.getElementById('span')
 const DolarP = document.getElementById('P')
+const selec = document.getElementById('select')
+
+const dolar = 5.68 
+const euro = 7.68
+
+selec.addEventListener('change', changeCoin)
   
 function calcValue(){
     let res = value.value / dolar
-    Span.innerHTML = value.value
-    DolarP.innerHTML = res.toFixed(2)
+    Span.innerHTML = new Intl.NumberFormat("pt-BR",{
+        style:"currency",
+        currency:"BRL",
+    }).format(value.value)
+
+
+    DolarP.innerHTML =  new Intl.NumberFormat("en-US",{
+        style:"currency",
+        currency:"USD",
+    }).format(res.toFixed(2))
+    
 }
+
+function changeCoin(){
+    const imgC = document.getElementById('a')
+    const nome = document.getElementById('name')
+    const change = selec.value
+
+    if (change === "Є Euro") {
+        imgC.src ="./assets/euro.png"
+        nome.innerHTML = "Euro"   
+    }
+
+    
+    
+}
+
