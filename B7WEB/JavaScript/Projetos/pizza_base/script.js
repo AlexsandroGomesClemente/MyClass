b6eb14dec5d090859ba6d7c3030cbb04c268518c
Alexsandro Gomes Clemente <alexsandro.gomes.clemente@hotmail.com>
2022-01-11 21:24:49 -0300

    Projeto Pizza.JS

---
+++ B7WEB/JavaScript/Projetos/pizza_base/script.js
@@ -0,0 +1,64 @@
+let modalQt = 1
+
+const c = (e) => document.querySelector(e)
+const cs = (e) => document.querySelectorAll(e)
+
+pizzaJson.map((pizza, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true)
+
+    // Informações da pizza 
+    pizzaItem.setAttribute('data-key', index)
+    pizzaItem.querySelector('.pizza-item--img img').src=pizza.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${pizza.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = pizza.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML =pizza.description
+    pizzaItem.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault()
+        modalQt = 1
+        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+
+        c('.pizzaBig img').src = pizzaJson[key].img
+        c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+        c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`
+        c('.pizzaInfo--size.selected').classList.remove('selected')
+        cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if (sizeIndex == 2) {
+                size.classList.add('selected')
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+        })
+        c('.pizzaInfo--qt').innerHTML = modalQt
+
+        c('.pizzaWindowArea').style.opacity = '0'
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(()=>{
+            c('.pizzaWindowArea').style.opacity = '1'
+        }, 200)
+
+
+
+    })
+
+
+
+
+
+
+    //Fim Informações da pizza
+    c('.pizza-area').append(pizzaItem)
+})  
+
+// Eventos do Modal 
+
+const closeModal = ()=> {
+
+    c('.pizzaWindowArea').style.opacity = '0'
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none'
+
+    }, 500)
+
+
+
+}
